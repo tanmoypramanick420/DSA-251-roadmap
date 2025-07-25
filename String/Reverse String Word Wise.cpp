d0a2a2849075b768465b4f60d2276309ddbbea28
TANMOY PRAMANICK <pramanickt15@gmail.com>
2025-07-25 20:55:38 +0530

    Update Reverse String Word Wise.cpp

--- String/Reverse String Word Wise.cpp
+++ String/Reverse String Word Wise.cpp
@@ -1,29 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string reverseWords(string input) {
+string reverseWords(string input) 
+{
         vector<string> str;
         int size = input.length();
-        for (int i = size - 1; i >= 0; i--) {
+        for (int i = size - 1; i >= 0; i--) 
+	{
             string temp = "";
-            while (i >= 0 && input[i] != ' ') {
+            while (i >= 0 && input[i] != ' ') 
+	    {
                 temp.push_back(input[i]);
                 i--;
             }
             reverse(temp.begin(), temp.end());
-            if (temp != "") {
+            if (temp != "") 
+	    {
                 str.push_back(temp);
             }
         }
 
         string rev = "";
-        for (int i = 0; i < str.size(); i++) {
+        for (int i = 0; i < str.size(); i++) 
+	{
             rev.append(str[i]);
             if (i < str.size() - 1)
                 rev.push_back(' ');
         }
         return rev;
-    }
+}
 
 int main() {
     string demo;
